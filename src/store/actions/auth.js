e2d9a4366c9e82692ebbd16a5b6c4b311209cdf1
Julio Cesar Ceron <julio.ceorn@gmail.com>
2018-05-01 18:43:56 -0300

    verify login

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,8 +1,38 @@
-import { TRY_AUTH } from './actionTypes';
+import { TRY_AUTH} from './actionTypes';
+import { uiStartLoading, uiStopLoading } from '.';
+import startMainTabs from '../../screens/MainTabs/startMainTabs';
 
 export const tryAuth = (authData) => {
-    return {
-        type: TRY_AUTH,
-        authData: authData
+    return dispatch => {
+       dispatch(authSignup(authData))
     };
 };
+
+export const authSignup = (authData) => {
+    return dispatch => {
+        dispatch(uiStartLoading())
+        fetch('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDU3wNx6Ky0ZxSiNBbd6kbx9ZGHvfmxPsY', {
+            method: 'POST',
+            body: JSON.stringify({
+                email: authData.email,
+                password: authData.password, 
+                returnSecureToken: true
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).catch(err => {
+            console.log('err', err);
+            dispatch(uiStopLoading())
+            alert('Authentication failed, please try again!')
+        }).then(res => res.json())
+        .then(parseRes => {
+            dispatch(uiStopLoading())
+            if (parseRes.error) {
+                alert('Authentication failed, please try again!')
+            } else {
+                startMainTabs()
+            }
+        })
+    }
+}
